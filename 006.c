b824bdc8ff23cdaf67fadab6038da4f7dc084f0c
Piotr Oleskiewicz <piotr@oleskiewi.cz>
2019-05-26 14:55:46 +0100

    implement reduced summation
    it's slower though

--- 006.c
+++ 006.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-long int
-diff_sqsum_sumsq(int n) {
-	long int sqsum = 0, sumsq = 0;
+long long int
+diff_sqsum_sumsq1(int n) {
+	long long int sqsum = 0, sumsq = 0;
 	for(int i = 1; i <= n; i++) {
 		sqsum += i;
 		sumsq += i*i;
@@ -12,9 +12,20 @@ diff_sqsum_sumsq(int n) {
 	return sqsum - sumsq;
 }
 
+long long int
+diff_sqsum_sumsq2(int n) {
+	long long int d = 0;
+	for(int i = 1; i <= n; i++) {
+		for(int j = 1; j < i; j++) {
+			d += 2*i*j;
+		}
+	}
+	return d;
+}
+
 int
 main(int argc, char *argv[]) {
 	int n = atoi(argv[1]);
-	printf("%ld\n", diff_sqsum_sumsq(n));
+	printf("%lld\n", diff_sqsum_sumsq1(n));
 	return 0;
 }
